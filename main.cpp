142a3c8a35a0baf3b39d90c309f291ce634c4859
Eli Laird <elaird03@outlook.com>
2020-01-27 17:42:56 -0600

    Created week one assignment

--- main.cpp
+++ main.cpp
@@ -2,10 +2,20 @@
 #include <fstream>
 
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::ofstream fout("output01.txt");
-    fout << "Hello world!" << std::endl;
+int main(int argc, char **argv) {
+    std::cout << "Welcome to Lab 1 of CS2341!" << std::endl;
+
+    std::ifstream fin(argv[1]);
+    std::ofstream fout(argv[2]);
+    int n;
+
+    while(fin >> n){
+      std::cout << n << std::endl;
+      fout << n << std::endl;
+    }
+
+    fin.close();
     fout.close();
+
     return 0;
 }
