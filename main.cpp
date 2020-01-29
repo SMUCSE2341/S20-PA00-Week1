b46e5caa9cb4c51748553b9a0c4595fded8c0b05
Patrick DeVries <39521214+PatrickDeVries@users.noreply.github.com>
2020-01-29 17:39:47 -0600

    Revert "calculated average"
    This reverts commit 36e9727e73481b1e9fd86b0fb8c0c573aaba946c.

--- main.cpp
+++ main.cpp
@@ -8,22 +8,13 @@ int main(int argc, char **argv) {
     std::ifstream fin(argv[1]);
     std::ofstream fout(argv[2]);
     int n;
-    int sum = 0;
-    int count = 0;
-
-
-
-
 
     while(fin >> n){
       std::cout << n << std::endl;
       fout << n << std::endl;
-      count++;
-      sum+=n;
     }
 
-    std::cout << int(sum/count) << std::endl;
-    fout << int(sum/count) << std::endl;
+
     int test = 0;
     fin.close();
     fout.close();
